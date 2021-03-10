b0a31a241c9f2c846152a0f9c1c0e67afc7eebb2
Zihan Xue <zihan.x@foxmail.com>
2021-03-09 20:31:31 -0800

    implement trie

--- Wurd/StudentSpellCheck.cpp
+++ Wurd/StudentSpellCheck.cpp
@@ -6,8 +6,12 @@ SpellCheck* createSpellCheck() {
 	return new StudentSpellCheck;
 }
 
+StudentSpellCheck::StudentSpellCheck() {
+    m_trie = new Trie();
+}
+
 StudentSpellCheck::~StudentSpellCheck() {
-	// TODO
+    delete m_trie;
 }
 
 bool StudentSpellCheck::load(std::string dictionaryFile) {
@@ -20,4 +24,66 @@ bool StudentSpellCheck::spellCheck(std::string word, int max_suggestions, std::v
 
 void StudentSpellCheck::spellCheckLine(const std::string& line, std::vector<SpellCheck::Position>& problems) {
 	// TODO
+}
+
+//
+// Trie implementation
+//
+
+StudentSpellCheck::Trie::Trie() {
+    root = new TrieNode();
+}
+
+StudentSpellCheck::Trie::~Trie() {
+    freeTrie(root);
+}
+
+bool StudentSpellCheck::Trie::insert(std::string word) {
+    TrieNode* cur = root;
+    for (char c : word) {
+        int i = indexOf(c);
+        if (cur->children[i] != nullptr) {
+            cur = cur->children[i];
+        }
+        else {
+            cur->children[i] = new TrieNode();
+            cur = cur->children[i];
+        }
+    }
+    if (cur->isWord) {
+        return false;
+    }
+    else {
+        cur->isWord = true;
+        return true;
+    }
+}
+
+bool StudentSpellCheck::Trie::find(std::string word) const {
+    TrieNode* cur = root;
+    for (char c : word) {
+        int i = indexOf(c);
+        if (cur->children[i] == nullptr) return false;
+        cur = cur->children[i];
+    }
+    return cur->isWord;
+}
+
+void StudentSpellCheck::Trie::freeTrie(TrieNode* root) {
+    for (TrieNode* i : root->children) {
+        if (i != nullptr)
+            freeTrie(i);
+    }
+    delete root;
+}
+
+//
+// TrieNode Implementation
+//
+
+StudentSpellCheck::Trie::TrieNode::TrieNode()
+: isWord(false) {
+   for (int i = 0; i < 27; i++) {
+       children[i] = nullptr;
+   }
 }
--- Wurd/StudentSpellCheck.h
+++ Wurd/StudentSpellCheck.h
@@ -6,16 +6,43 @@
 #include <string>
 #include <vector>
 
+const std::string chars = "abcdefghijklmnopqrstuvwxyz'";
+
 class StudentSpellCheck : public SpellCheck {
 public:
-    StudentSpellCheck() { }
+    StudentSpellCheck();
 	virtual ~StudentSpellCheck();
 	bool load(std::string dict_file);
 	bool spellCheck(std::string word, int maxSuggestions, std::vector<std::string>& suggestions);
 	void spellCheckLine(const std::string& line, std::vector<Position>& problems);
 
 private:
-
+    // TODO: move these
+    class Trie {
+    public:
+        Trie();
+        ~Trie();
+        bool insert(std::string word);
+        bool find(std::string word) const;
+    private:
+        struct TrieNode {
+            TrieNode();
+            TrieNode* children[27];
+            bool isWord;
+        };
+        
+        TrieNode* root;
+        
+        // unsafe to assume ASCII scheme,
+        // char values might not be consecutive i.e. 'b' - 'a' might not be 1
+        // thus use a separate function to map char to int
+        int indexOf(char c) const {
+            return chars.find(c);
+        }
+        void freeTrie(TrieNode* root);
+    };
+    
+    Trie* m_trie;
 };
 
 #endif  // STUDENTSPELLCHECK_H_
